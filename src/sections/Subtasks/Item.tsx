e8f461989ddf54b3575d8650350c6d62b757c2d2
Alexander Navarro <ale.navarro.parra@gmail.com>
2022-06-26 18:00:35 -0400

    add subtask item layout

--- src/sections/Subtasks/Item.tsx
+++ src/sections/Subtasks/Item.tsx
@@ -1,5 +1,38 @@
-import React from "react";
+import style from "./Item.module.css";
 
-export const Item = (props: {}) => {
-  return <div>Item</div>;
+interface Props {
+  idx: number;
+}
+
+export const Item = ({ idx }: Props) => {
+  return (
+    <div className={[style.item, style.grid].join(" ")}>
+      <div className={style.group}>
+        <label htmlFor={`subtask-${idx}-name`}>Name</label>
+        <input type="text" id={`subtask-${idx}-name`} name="task-name" />
+      </div>
+
+      <div className={style.buttons_container}>
+        <button>Copy</button>
+        <button>Delete</button>
+      </div>
+
+      <div className={style.group}>
+        <label htmlFor={`subtask-${idx}-dificulty`}>Name</label>
+        <select id={`subtask-${idx}-dificulty`} name="dificulty"></select>
+      </div>
+      <div className={style.group}>
+        <label htmlFor={`subtask-${idx}-name`}>Name</label>
+        <select id={`subtask-${idx}-dificulty`} name="dificulty"></select>
+      </div>
+      <div className={style.group}>
+        <label htmlFor={`subtask-${idx}-hours`}>Time (hours)</label>
+        <div>{12}</div>
+      </div>
+      <div className={style.group}>
+        <label htmlFor={`subtask-${idx}-minutes`}>Time (minutes)</label>
+        <div>{12}</div>
+      </div>
+    </div>
+  );
 };
--- src/sections/Subtasks/List.tsx
+++ src/sections/Subtasks/List.tsx
@@ -1,19 +1,27 @@
 import { actions, useSubtasksContext } from "../../Context/useSubtask";
 import { Item } from "./Item";
+import style from "./List.module.css";
 
 export const SubtaskList = () => {
   const { data, dispatch } = useSubtasksContext();
 
   return (
-    <div>
+    <div id={style.subtasks}>
       <div className="hstack justify-content-between">
         <h2>Subtasks</h2>
-        <button onClick={() => dispatch({ type: actions.ADD })}>Add</button>
+        <button
+          id={style.addButton}
+          onClick={() => dispatch({ type: actions.ADD })}
+        >
+          Add
+        </button>
       </div>
 
-      {data.subtasks.map((_item, idx) => (
-        <Item key={idx} />
-      ))}
+      <div className={style.list}>
+        {data.subtasks.map((_item, idx) => (
+          <Item key={idx} idx={idx} />
+        ))}
+      </div>
     </div>
   );
 };
